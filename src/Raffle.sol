2fc9d48df4e8a373e195ee678e1a0a33498b7f4e
Lokesh0224 <lokeshk20019@gmail.com>
2025-07-27 17:24:35 +0530

    revert is more gas efficient than reqiure

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -32,14 +32,24 @@ pragma solidity ^0.8.0;
  * @author Lokesh
  * @notice This contract allows users to enter a raffle and win a prize.
  */
+
+
 contract Raffle {
+    //Errors
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entraceFee;
 
     constructor(uint256 entranceFee){
         i_entraceFee= entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entraceFee, "Not enough Eth");
+        // require(msg.value >= i_entraceFee, SendMoreToEnterRaffle()); MORE READABLE
+        if(msg.value < i_entraceFee) revert Raffle__SendMoreToEnterRaffle();
+
+    }
 
     function pickWinner() public {}
 
