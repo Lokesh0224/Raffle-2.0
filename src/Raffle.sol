3642f622b9bf920ccc03c4ab892fbc2aef677cac
Lokesh0224 <lokeshk20019@gmail.com>
2025-07-29 22:39:07 +0530

    Transferred the Raffle winner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -37,6 +37,7 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 contract Raffle is VRFConsumerBaseV2Plus {
     //Errors
     error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
 
     uint16 private constant REQUEST_CONFIRMATION = 3;
     uint32 private constant NUM_WORDS = 1;
@@ -47,6 +48,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
     uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
+    address payable private s_recentWinner;
 
     //Events
     event RaffleEntered(address indexed player);
@@ -100,7 +102,18 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {//we got the random number
+        //s_player= 10
+        //randomNum= 345983459345
+        //345983459345%10=34598345934
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, )= recentWinner.call{value: address(this).balance}("");
+        if(!success){
+            revert Raffle__TransferFailed();
+        }
+    } 
 
     //Getter functions
     function getEntranceFee() external view returns (uint256) {
