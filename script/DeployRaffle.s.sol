98845358b15a09329814a5fd4111aedb0db7f747
Lokesh0224 <lokeshk20019@gmail.com>
2025-07-31 19:42:16 +0530

    deployContract() should be public not internal

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -8,7 +8,7 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 contract DeployRaffle is Script {
     function run() public {}
 
-    function deployContract() internal returns (Raffle, HelperConfig) {
+    function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
